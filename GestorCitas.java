38687ea9c140121b7561099f99899ebf2cd0a2de
Benjamin <b.valenzuela06@ufromail.cl>
2025-03-31 23:14:04 -0300

    Creacion GestorCitas

---
+++ GestorCitas.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorCitas(){
+    private List<CitaMedica> citas = new ArrayList<>();
+
+    public void agregarCita(CitaMedica cita){
+        citas.add(cita);
+    }
+
+    public void eliminarCita(String fecha, String hora){
+        citas.removeIf(c -> c.getFecha().equals(fecha) && c.getHora().equals(hora));
+    }
+
+    public List<CitaMedica> mostrarCitas(){
+        return citas;
+    }
+
+    public CitaMedica buscarCita(String fecha, String hora){
+        return citas.stream()
+            .filter(C -> C.getFecha().equals(fecha) && C.getHora().equals(hora))
+            .findFirst()
+            .orElse(null);
+    }
+}
