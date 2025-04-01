3b532f62294692cdab28277699a6a8d34946c481
Erick Soto <osodaerick@gmail.com>
2025-03-31 23:19:41 -0300

    Corrección de errores minimos

--- GestorCitas.java
+++ GestorCitas.java
@@ -1,13 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class GestorCitas(){
+public class GestorCitas{
     private List<CitaMedica> citas = new ArrayList<>();
-
     public void agregarCita(CitaMedica cita){
         citas.add(cita);
     }
-
     public void eliminarCita(String fecha, String hora){
         citas.removeIf(c -> c.getFecha().equals(fecha) && c.getHora().equals(hora));
     }
